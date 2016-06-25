ce9915434688557caf462356c4b956e6a06bd746
Chris Lankford <christopherlankford@gmail.com>
2016-06-25 09:18:09 -0700

    Implemented a child component. Styling is all messed up

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
+const Product = React.createClass({
+    render: function() {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src='images/products/image-aqua.png' />
+                </div>
+                <div class='middle aligned content'>
+                    <div className='description'>
+                        <a>Product Number 1</a>
+                        <p>A description of product number 1.</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img
+                            className='ui avatar image'
+                            src='images/avatars/daniel.jpg'
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
 const ProductList = React.createClass({
    render: function() {
        return (
            <div class='ui items'>
-               Hello World!
+               <Product />
            </div>
        );
    } 
@@ -13,8 +38,3 @@ ReactDOM.render(
     document.getElementById('content')
 );
 
-const Product = React.createClass({
-    render: function() {
-        return (<div></div>)
-    }
-});
