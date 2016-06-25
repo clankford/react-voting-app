52f05a02e28e80adda720b2b89c6ca436c2ce37c
Chris Lankford <christopherlankford@gmail.com>
2016-06-24 17:27:23 -0700

    Added basic component structure

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+const ProductList = React.createClass({
+   render: function() {
+       return (
+           <div class='ui items'>
+               Hello World!
+           </div>
+       );
+   } 
+});
